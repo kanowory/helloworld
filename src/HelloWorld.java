0b1cdaae89f7c6301a1117558a2588bef5b3e9cb
Katarzyna <kanowory@student.wszib.edu.pl>
2022-11-17 12:41:40 +0100

    Dodano pętle oraz wyświetlanie napisu wielkimi literami.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
+
+        String helloWorld = "Hello World!";
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(helloWorld.toUpperCase());
+        }
     }
 }
